5ed7bc69445eb6299ba9b3aa4fc96fe3b10a388e
ekhalturin-ki15 <1tigrin@mail.ru>
2021-09-26 16:42:06 +0700

    17

--- New_Project.cpp
+++ New_Project.cpp
@@ -12,29 +12,26 @@ int main()
 
 	int h, w;
 
-	cin >> h >> w;
+	cin >> h;
 	int** matrix;
+	int* weight; 
 
 	matrix = new int* [h];
-	for (int i = 0; i < h; ++i)
-	{
-		matrix[i] = new int [w];
-	}
-
+	weight = new int [h];
 
 	for (int y = 0; y < h; ++y)
 	{
-		for (int x = 0; x < w; ++x)
+		cin >> weight[y];
+		matrix[y] = new int [ weight[y] ];
+		for (int x = 0; x < weight[y]; ++x)
 		{
 			cin >> matrix[y][x];
 		}
 	}
 
-
-
 	for (int y = 0; y < h; ++y)
 	{
-		for (int x = w -1 ; x >=0; --x)
+		for (int x = weight[y] -1 ; x >=0; --x)
 		{
 			cout << matrix[y][x] << " ";
 		}
@@ -43,14 +40,41 @@ int main()
 
 
 
-
-
 	for (int i = 0; i < h; ++i)
 	{
 		delete [] matrix[i];
 	}
 	delete[] matrix;
+	delete[] weight;
+
+	cout << "\n";
+
+	cin >> h;
+	vector<vector<int>> v;
+	v.resize(h);
+
+	int a;
+	for (int y = 0; y < h; ++y)
+	{
+		cin >> a;
+		v[y].resize(a);
+		for (int x = 0; x < v[y].size(); ++x)
+		{
+			cin >> v[y][x];
+		}
+	}
+
+	for (int y = h - 1; y >= 0; --y)
+	{
+		for (int x = 0; x < v[y].size(); ++x)
+		{
+			cout << v[y][x] << " ";
+		}
+		cout << "\n";
+	}
 
 	return 0;
+
+
 
 }
