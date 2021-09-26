a5e2e98d181fef1984e34dd6e867dcc037a376e2
ekhalturin-ki15 <1tigrin@mail.ru>
2021-09-26 16:17:36 +0700

    5

--- New_Project.cpp
+++ New_Project.cpp
@@ -22,50 +22,72 @@ int main()
 
 	int static_mas[1000];
 
-	// [0 n)
-
-	// [10 20]  = 11
-	// (10 20] = 20 - 10
-	// (10 20) = 9
-	// [123 1024] = 902 = 1024 - 123 + 1
-
-	{
-		//int a = 0;
-		//[ 0 n )
-
-		//int i = 0;
-		//for (; i < n; ++i)
-		//{
-		//	cin >> v[i];
-		//	a *= a;
-		//}
-		//int i = 0
-		// if (i < n)
-		// cin >> v[i];
-		// a = a*a
-		// ++i
-		// if (i < n)
-		// cin >> v[i];
-	}
-
-	{
-		/*int i = 0, a = 0;
-		while (i < n)
-		{
-			cin >> v[i];
-			a *= a;
-			++i;
-		}*/
-	}
-
 	/*int i = 0, a = 0;
-
-	do
+	while (true)
 	{
+		if (i >= n) break;
 		cin >> v[i];
 		a *= a;
+
 		++i;
-	} while (i < n);*/
+	}*/
+
+
+	for (int i = 0; i < n; ++i) cin >> v[i];
+
+
+	long long mul = 1, add = 0;
+	for (int i = n - 1; i >=0; --i) add += v[i];
+
+	//cout << add;
+
+	// сортировка слиянием merge
+	// быстрая сортировка
+	// пирамидальная
+	// пузырьком
+	// подсчётом
+
+	// O(n^2)
+	// n + (n-1) + .. + 1 = (n)*(n+1)/2
+
+	// <-------
+	//7 233  5 10 993 1827 827 5 2        
+	//233 7 10 993 1827  827 5 2
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int i = 0; i < n; ++i) cout << v[i] << " ";
+		cout << "\n";
+		for (int j = 0; j < n - i - 1; ++j)
+		{
+			if (v[j] < v[j + 1])
+			{
+				//swap(v[j], v[j + 1]); // 7 10
+				//int save; 
+				//save = v[j];  // 7
+				//v[j] = v[j + 1];  // 10 10
+				//v[j + 1] = save; // 10 7
+
+				//v[j] ^= v[j + 1] ^= v[j] ^= v[j + 1];
+
+				v[j] = v[j] ^ v[j + 1]; // Зелёный
+				v[j + 1] = v[j + 1] ^ v[j];
+				v[j] = v[j] ^ v[j + 1];
+			}
+		}
+
+	}
+
+
+	//int matrix[100][1000][12][2][2]; // 4*100*1000*12*2*2
+
+
+	cin >> n;
+	int** matrix;
+
+	//matrix = new int* [100];
+
 
 	return 0;
 
