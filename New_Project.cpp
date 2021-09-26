61699b577c4dba27545d8c465ef9e1a3d5058873
ekhalturin-ki15 <1tigrin@mail.ru>
2021-09-26 16:25:09 +0700

    6

--- New_Project.cpp
+++ New_Project.cpp
@@ -10,84 +10,46 @@ int main()
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	int n;
+	int h, w;
 
-	cin >> n;
+	cin >> h >> w;
+	int** matrix;
 
-	int* dy_mas;
-
-	dy_mas = new int[n];
-
-	vector<int> v(n);
-
-	int static_mas[1000];
-
-	/*int i = 0, a = 0;
-	while (true)
+	matrix = new int* [h];
+	for (int i = 0; i < h; ++i)
 	{
-		if (i >= n) break;
-		cin >> v[i];
-		a *= a;
-
-		++i;
-	}*/
-
-
-	for (int i = 0; i < n; ++i) cin >> v[i];
-
-
-	long long mul = 1, add = 0;
-	for (int i = n - 1; i >=0; --i) add += v[i];
-
-	//cout << add;
-
-	// сортировка слиянием merge
-	// быстрая сортировка
-	// пирамидальная
-	// пузырьком
-	// подсчётом
-
-	// O(n^2)
-	// n + (n-1) + .. + 1 = (n)*(n+1)/2
-
-	// <-------
-	//7 233  5 10 993 1827 827 5 2        
-	//233 7 10 993 1827  827 5 2
-
-
-	for (int i = 0; i < n; ++i)
-	{
-		for (int i = 0; i < n; ++i) cout << v[i] << " ";
-		cout << "\n";
-		for (int j = 0; j < n - i - 1; ++j)
-		{
-			if (v[j] < v[j + 1])
-			{
-				//swap(v[j], v[j + 1]); // 7 10
-				//int save; 
-				//save = v[j];  // 7
-				//v[j] = v[j + 1];  // 10 10
-				//v[j + 1] = save; // 10 7
-
-				//v[j] ^= v[j + 1] ^= v[j] ^= v[j + 1];
-
-				v[j] = v[j] ^ v[j + 1]; // Зелёный
-				v[j + 1] = v[j + 1] ^ v[j];
-				v[j] = v[j] ^ v[j + 1];
-			}
-		}
-
+		matrix[i] = new int [w];
 	}
 
 
-	//int matrix[100][1000][12][2][2]; // 4*100*1000*12*2*2
+	for (int y = 0; y < h; ++y)
+	{
+		for (int x = 0; x < w; ++x)
+		{
+			cin >> matrix[y][x];
+		}
+	}
 
 
-	cin >> n;
-	int** matrix;
 
-	//matrix = new int* [100];
+	for (int y = 0; y < h; ++y)
+	{
+		for (int x = w -1 ; x >=0; --x)
+		{
+			cout << matrix[y][x] << " ";
+		}
+		cout << "\n";
+	}
 
+
+
+
+
+	for (int i = 0; i < h; ++i)
+	{
+		delete [] matrix[i];
+	}
+	delete[] matrix;
 
 	return 0;
 
