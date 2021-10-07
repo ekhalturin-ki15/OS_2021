793303119def4769424036ee1923d5d08bccc4c2
ekhalturin-ki15 <1tigrin@mail.ru>
2021-10-07 19:50:59 +0700

    1

--- New_Project.cpp
+++ New_Project.cpp
@@ -1,80 +1,117 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
+#include <cmath>
 
 using namespace std;
 
-int main()
+void my_swap(int& left, int& right)
+{
+    // ++i; i = i +1;
+    // i += 1;
+
+    left ^= right ^= left ^= right;
+}
+
+void cin_vector(vector<int>& eng)
+{
+    int n;
+    cin >> n;
+    eng.resize(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> eng[i];
+    }
+}
+
+void cout_vector(vector<int>& v)
+{
+    for (int i = 0; i < v.size(); ++i)
+        cout << ++v[i] << "  ";
+    cout << "\n";
+}
+
+
+
+void function(int i, vector<int>& v)
+{
+    v[i] = i;
+
+    if (i < v.size())
+    {
+        function(i + 1, v);
+    }
+
+}
+
+int mod_double(double d, const int mod)
+{
+    return (int(d) % mod);
+}
+
+
+string result(int& l, int r, const double& c)
+{
+    double tmp = double(l * r) + c;
+
+    if (mod_double(tmp, 3) == 0)
+        return "*\t";
+    else
+        return to_string(tmp) + "\t";
+
+}
+
+bool intersect(int a, int b)
 {
 
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    std::string sa = to_string(atoi("52925454"));//std::to_string(a);
+    std::string sb = to_string(atoi("292"));//std::to_string(b);
 
-	int h, w;
 
-	cin >> h;
-	int** matrix;
-	int* weight; 
+    if (sa.size() < sb.size()) swap(sa, sb);
 
-	matrix = new int* [h];
-	weight = new int [h];
 
-	for (int y = 0; y < h; ++y)
-	{
-		cin >> weight[y];
-		matrix[y] = new int [ weight[y] ];
-		for (int x = 0; x < weight[y]; ++x)
-		{
-			cin >> matrix[y][x];
-		}
-	}
+    for (int x = 0; x < sa.size(); ++x)
+    {
+        bool flag = true;
+        for (int y = 0; y < sb.size(); ++y)
+        {
+            if (sa[x + y] != sb[y])
+            {
+                flag = false;
+                break;
 
-	for (int y = 0; y < h; ++y)
-	{
-		for (int x = weight[y] -1 ; x >=0; --x)
-		{
-			cout << matrix[y][x] << " ";
-		}
-		cout << "\n";
-	}
+            }
+
+
+        }
+        if (flag) return true;
+
+    }
 
 
 
-	for (int i = 0; i < h; ++i)
-	{
-		delete [] matrix[i];
-	}
-	delete[] matrix;
-	delete[] weight;
+    return false;
+}
 
-	cout << "\n";
 
-	cin >> h;
-	vector<vector<int>> v;
-	v.resize(h);
+int main()
+{
+    int a, b;
 
-	int a;
-	for (int y = 0; y < h; ++y)
-	{
-		cin >> a;
-		v[y].resize(a);
-		for (int x = 0; x < v[y].size(); ++x)
-		{
-			cin >> v[y][x];
-		}
-	}
+    cin >> a >> b;
 
-	for (int y = h - 1; y >= 0; --y)
-	{
-		for (int x = 0; x < v[y].size(); ++x)
-		{
-			cout << v[y][x] << " ";
-		}
-		cout << "\n";
-	}
+    intersect(a, b);
 
-	return 0;
 
+    if (intersect(a, b))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 
 
 }
