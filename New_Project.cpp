b2b7c530fa105fb1ea5fe5214f4a4d72d1b195a3
ekhalturin-ki15 <1tigrin@mail.ru>
2021-10-10 18:27:37 +0700

    last

--- New_Project.cpp
+++ New_Project.cpp
@@ -5,81 +5,40 @@
 
 using namespace std;
 
-
-void cout_ans(const vector<int>& digit, int now_size)
+struct my_vector
 {
-	for (int i = 0; i < now_size; ++i)
-		cout << digit[i] << " ";
+public:
 
-	cout << "\n";
-
-	return;
-}
-
-
-// step - ячейка, куда записываем
-// number - сколько осталось
-// max_choose - ранее макс исп число
-// digit - записываем ответ
-
-
-// 1 5 1
-void recursion(int i, int number, int min_choose, vector<int>& digit, int& now)
-{
-	if (number == 0)
+	my_vector(int n)
 	{
-		cout << now << " | ";
-		++now;
-		cout_ans(digit, i);
-		return;
+		mas = new int[n];
 	}
 
-	int upper_limit = min(number, min_choose);	//5 != 3 2
-	for (int now_number = 1; now_number <= upper_limit; ++now_number) // 1..6 //1..1 // 1..1
+	~my_vector()
 	{
-		digit[i] = now_number;
-		recursion(i + 1, number - now_number, now_number, digit, now);
+		delete[] mas;
 	}
 
-	return;
-}
+	void resize(int n)
+	{
+		mas = new int[n];
+	}
+
+
+private:
+	int size = 0;
+
+	int* mas;
+
+
+};
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
+	vector<int> v(50);
+
 	int n;
-
-	cin >> n;
-
-	vector<int> v(n);
-	int now = 0;
-
-	recursion(0, n, n, v, now);
-	cout << "\n";
-
-	//// n = 3
-	////recursion(0, 3, 3, { 0, 0, 0 })
-	//{
-	//	int i = 0; int number = 3; int max_choose = 3; vector<int>& digit = v;
-
-	//	int upper_limit = 3;
-
-	//	digit[i] = 1;
-	//	recursion(1, number - 1, now_number, digit);
-
-	//	digit[i] = 2;
-	//	recursion(1, number - 2, now_number, digit);
-
-	//	digit[i] = 3;
-	//	recursion(1, number - 3, now_number, digit);
-
-
-	
-
-	
-
-
-
 }
