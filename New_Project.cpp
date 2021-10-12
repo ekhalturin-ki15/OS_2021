257fdc599ecfbca922d2c51302d7dc8da80a123b
ekhalturin-ki15 <1tigrin@mail.ru>
2021-10-12 23:56:22 +0700

    1

--- New_Project.cpp
+++ New_Project.cpp
@@ -5,39 +5,104 @@
 
 using namespace std;
 
-struct my_vector
+enum Stage
 {
-public:
+	pause, start, end = 1, strange
+};
 
-	my_vector(int n)
-	{
-		mas = new int[n];
-	}
+enum Champer
+{
+	angry, hunger
+};
 
-	~my_vector()
-	{
-		delete[] mas;
-	}
 
-	void resize(int n)
-	{
-		mas = new int[n];
-	}
+struct Array
+{
+	int* date;
 
+
+
+
+};
+
+
+struct Human
+{
 
 private:
-	int size = 0;
+	string cash; // поля
+	string name; // поля
+	string sur_name;// поля
 
-	int* mas;
+	int age;// поля
+
+public:
 
 
+	Champer my_champer;// поля
+
+	void CinDate() // методы
+	{
+		int a;
+		cin >> name >> sur_name >> age >> a >> cash;
+
+		my_champer = static_cast<Champer>(a);
+
+	}
+
+	string OutDate_1() // Метод
+	{
+		string name = "404";
+		string ret = name + " " + sur_name + "\n";
+
+
+		return ret;
+	}
+	
+	
+	string OutDate_2() // Метод
+	{
+		string name = "404";
+		string ret = this->name + " " + sur_name + "\n";
+		//(*this).name; == this->name;
+
+		(*this).cash = 1000;
+
+		return ret;
+	}
+
+} first;
+
+
+
+
+struct New_Struct
+{
+	int a;
+	int b, c;
 };
 
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	
 
+	New_Struct new_struct; /*экземпляр класс*/
+
+	//cin >> new_struct.a >> new_struct.b >> new_struct.c;
+
+	//New_Struct second_struct;
+
+	//cin >> second_struct.a >> second_struct.b >> second_struct.c;
+
+	Human my_human, my_human_2;
+
+	my_human.CinDate();
+
+	cout << my_human.OutDate_1();
+
+	cout << my_human.OutDate_2();
+
+	//my_human_2.CinDate();
 
 }
