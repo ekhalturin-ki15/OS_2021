57c612a096303f66b51b69f02dab55dd5f0b8572
ekhalturin-ki15 <1tigrin@mail.ru>
2021-10-17 16:14:24 +0700

    4

--- New_Project.cpp
+++ New_Project.cpp
@@ -6,6 +6,8 @@
 #include <list>
 #include <forward_list>
 #include <queue>
+#include <stack>
+#include <set>
 
 using namespace std;
 
@@ -38,33 +40,35 @@ int main()
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	list<int> ls({ 5, 6, 7, 8, 9, 10, 11, 9, 1, 9, 23, 43, 54, 9, 64});
 
-	//list<int>::iterator it = find(ls.begin(), ls.end(), 123);
+	auto a = 5ull;
+	//list<int> ls({ 5, 6, 7, 8, 9, 10, 11, 9, 1, 9, 23, 43, 54, 9, 64});
 
-	fuct(ls);
+	////list<int>::iterator it = find(ls.begin(), ls.end(), 123);
 
-	ls.insert(ls.begin(), 100);
+	//fuct(ls);
 
-	ls.insert(ls.end(), 100);
+	//ls.insert(ls.begin(), 100);
 
-
-	forward_list<int> lsw;
-
-	vector<int> v;
-	sort(v.begin(), v.end());
+	//ls.insert(ls.end(), 100);
 
 
-	//sort(ls.begin(), ls.end());
-	ls.sort();
+	//forward_list<int> lsw;
+
+	//vector<int> v;
+	//sort(v.begin(), v.end());
 
 
-	list<int>::iterator it = ls.begin();
-	cout << "\n";
-	for (; it != ls.end(); ++it)
-	{
-		cout << (*it) << " ";
-	}
+	////sort(ls.begin(), ls.end());
+	//ls.sort();
+
+
+	//list<int>::iterator it = ls.begin();
+	//cout << "\n";
+	//for (; it != ls.end(); ++it)
+	//{
+	//	cout << (*it) << " ";
+	//}
 
 	queue<int> q({3,4,3,-23,32,59,29,32});
 
@@ -72,8 +76,41 @@ int main()
 	q.push(1);
 	q.push(4);
 
-
 	q.pop(); q.pop(); q.pop(); q.pop();
 	cout << q.front();
+
+	string s = "3435435";
+
+	s = s + s;
+
+
+	stack<int> st;
+
+	//for (auto it : q) cout << it << " ";
+
+	st.push(10); st.push(10); st.push(10); st.push(10);
+	st.pop();
+
+	st.top();
+
+
+	vector<int> v({435435,324,324,324}); // --insert --erase
+
+	v.push_back(100); // O(1)
+
+	v.pop_back(); v.pop_back();
+
+	v.front();
+
+	v.back();
+
+
+	deque<int> d;
+
+	//d[10];
+	//d.push_front();
+	//d.pop_front();
+
+
 
 }
