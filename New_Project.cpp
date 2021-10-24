a00d386938a830091b586394a63a728d937971a6
ekhalturin-ki15 <1tigrin@mail.ru>
2021-10-24 16:30:21 +0700

    15

--- New_Project.cpp
+++ New_Project.cpp
@@ -4,80 +4,6 @@
 
 using namespace std;
 
-struct Base
-{
-public:
-
-	string name;
-	string who = "Base";
-	int age;
-
-	Base() // По умолчания
-	{
-		name = "???";
-		age = 100;
-	}
-
-	Base(string n) : name(n), age(20)
-	{
-		//name = n;
-		//age = 20;
-	}
-
-	void Out()
-	{
-		cout << "\n" <<  name << " "<<age << " "<< who;
-	}
-
-	
-
-};
-
-
-
-struct Child : public Base 
-{
-public:
-
-	Child()
-	{
-		name = "&&&";
-		age = -1;
-		v = { 0, 0 ,0 };
-		who = "Child";
-	}
-
-	Child(string n) //: v({2, 3, 4})
-	{
-		name = n;
-		age = 1;
-		v = {2, 3 ,4}; 
-		who = "Child";
-	}
-
-	void Out()
-	{
-		cout << "\n" << name << " " << age << " " << who << "\n";
-
-		cout << " vector: ";  for (auto it : v) cout << it << " ";
-
-	}
-
-
-	vector<int> v;
-};
-
-
-
-struct Grandson : public Child
-{
-public:
-
-
-};
-
-
-
 //ostream& operator<<(ostream& out, vector<int> v)
 //{
 //	for (auto it : v)
@@ -86,6 +12,46 @@ public:
 //	return out;
 //}
 
+struct Node
+{
+	enum {_int, _char, _string} type;
+
+	//const int _int = 0;
+	//const int _char = 1;
+	//const int _string = 2;
+
+};
+
+struct Int: Node
+{
+	int data;
+	int& out() { return data; }
+};
+
+struct Char : Node
+{
+	char data;
+	char& out() { return data; }
+};
+
+struct String : Node
+{
+	string data;
+	string& out() { return data; }
+};
+
+//
+//
+//struct All
+//{
+//	int type;
+//
+//	int data1;
+//	char data2;
+//	string data3;
+//};
+
+// {1, "36565" , 43, '3', '2', "43434", 5, 3, 2}
 
 
 int main()
@@ -93,70 +59,39 @@ int main()
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
  
+	//Unit test
 
-	//Child ch;
+	vector<Node*> mas;
+	
+	int n;
+	cin >> n;
 
-	//ch.Out();
-
-
-	Child ch;
-
-	ch.Out();
-
-
-
-
-	Base* b;
-
-	b = new Base;
-
-	b->Out();
-
-	delete b;
-
-	b = new Child;
-
-	b->Out();
-
-	delete b;
-
-	const int size = 10;
-	void* v[size];
-
-	//{1, 1, 2 ,"cf", 32, 'a' , 'd', 'g'}
+	mas.resize(n);
 
 	srand(time(0));
-	
-	for (int i = 0; i < size; ++i)
-	{
-		int r = rand() % 2;
 
-		if (r == 0)
-		{
-			v[i] = new Child(to_string(i));
-			//cout << *v[i];
-		}
-		else
-		{
-			//v[i] = new Base(to_string(i));
-		}
+	for (int i = 0; i < n; ++i)
+	{
 
 	}
 
 
-	for (int i = 0; i < size; ++i)
-	{
-		(*(static_cast<Grandson*>(v[i]))).Out();
+
+	//vector<All> v(2);
+
+	//v[0].data1 = 1;
+	//v[0].type = 0;
+
+	//v[1].data3 = "36565";
+	//v[1].type = 2;
+
+
+	//for (int i = 0; i < 2; ++i)
+	//{
+
+	//}
 
 
 
-
-		// ->   ~	*().
-	}
-
-
-	//float f = 1 / (string)(5);
-
-
-	//Unit test 
+ 
 }
